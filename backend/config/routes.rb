b57090411e81d5a8434f4a7764ca0d5a2520b236
guchio33 <aokihikari3@gmail.com>
2022-06-26 23:38:15 +0900

    認証メール機能の追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+
+  #ログイン機能のルーティング
+  mount_devise_token_auth_for 'User', at: 'auth',controllers: {
+    registrations: 'auth/registrations'
+  }
+  #mログインユーザ取得のルーティング
+  namespace :auth do
+    resources :sessions, only: %i[index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
